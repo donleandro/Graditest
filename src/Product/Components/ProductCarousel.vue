b6b6efa9da51403dda3337a17660af8032d20774
Leandro Moreno <123857@gmail.com>
2022-03-15 19:08:42 -0500

    Create ProductCarousel.vue

---
+++ src/Product/Components/ProductCarousel.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="carousel">
+    <div class="carouse--containerl">
+      <img :src="featuredImage" class="carousel--featured-image"/>
+    </div>
+    <div class="carousel-nav">
+      <button v-for="(item, index) in images" :id="`product--image-${index}`" :key="index" class="carousel-nav--button"
+              @click="changeImage(index)">
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProductCarousel',
+  props: {
+    images: {
+      required: true
+    },
+    featured:{
+      type: String,
+      required: true
+    }
+  },
+  created() {
+    // this.featuredImage = this.featured;
+    this.updateFeaturedImage();
+  },
+  data() {
+    return {
+      activeSlide: 0,
+      featuredImage: ''
+    }
+  },
+  methods: {
+    nextSlide() {
+      this.activeSlide = this.activeSlide + 1;
+      this.updateFeaturedImage();
+    },
+    prevSlide() {
+      this.activeSlide = this.activeSlide - 1;
+      this.updateFeaturedImage();
+    },
+    changeImage(index) {
+      this.activeSlide = index;
+      this.updateFeaturedImage();
+    },
+    updateFeaturedImage() {
+      this.featuredImage = this.images[this.activeSlide];
+    }
+  }
+}
+</script>
