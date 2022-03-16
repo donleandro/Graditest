e1a8f3f39ecbd629fb0d5f22d27e72aa372572d0
Leandro Moreno <123857@gmail.com>
2022-03-15 19:08:57 -0500

    Create ProductTitle.vue

---
+++ src/Product/Components/ProductTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <span>$ {{discountPrice}}<small>{{ price }}</small></span>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'ProductTitle',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      },
+      discountPrice: {
+        type: Number,
+        required: true
+      }
+    }
+  }
+</script>
