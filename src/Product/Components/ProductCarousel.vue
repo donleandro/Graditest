2838ece00988682784f4276d99a9f3f48e0edb85
Leandro Moreno <123857@gmail.com>
2022-03-16 00:19:34 -0500

    Update ProductCarousel.vue
    Update in the styles for breakpoints

--- src/Product/Components/ProductCarousel.vue
+++ src/Product/Components/ProductCarousel.vue
@@ -3,11 +3,13 @@
     <div class="carouse--containerl">
       <img :src="featuredImage" class="carousel--featured-image"/>
     </div>
-    <div class="carousel-nav">
-      <button v-for="(item, index) in images" :id="`product--image-${index}`" :key="index" class="carousel-nav--button"
-              @click="changeImage(index)">
-      </button>
-    </div>
+    <ul class="carousel-nav">
+      <li v-for="(item, index) in images" :id="`product--image-${index}`" :key="index" :class="{'carousel-nav--button__select': featuredImage === item}"
+          class="carousel-nav--button"
+          @click="changeImage(index)">
+        <img :src="item" class="carousel-nav--image"/>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -17,7 +19,7 @@ export default {
     images: {
       required: true
     },
-    featured:{
+    featured: {
       type: String,
       required: true
     }
@@ -51,3 +53,58 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.carousel {
+  &-nav--button {
+    cursor: pointer;
+    display: inline-grid;
+    width: 111px;
+    /* height: 40px; */
+    /* border: 1px solid #d5d5d5; */
+    /* vertical-align: middle; */
+    color: #d5d5d5;
+    margin: 7px;
+
+    &__select {
+      border: 1px solid #d5d5d5;
+    }
+  }
+
+  &-nav--image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+.carousel--featured-image {
+  width: 100%;
+  object-fit: cover;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+@media (max-width: 768px) {
+  .carousel {
+    &-nav--button {
+      width: 16px;
+      height: 16px;
+      margin: 0 10px;
+      border-radius: 50%;
+      cursor: pointer;
+      position: relative;
+      background-color: white;
+      border: 1px solid black;
+      &__select {
+        background-color: black;
+      }
+      img {
+        display: none!important;
+      }
+    }
+  }
+}
+</style>
